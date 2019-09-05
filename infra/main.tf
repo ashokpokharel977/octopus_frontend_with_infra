812a97e033c0f69089a53ea106c9a078af19b230
ashokpokharel977 <ashokpokharel977@gmail.com>
2019-09-05 15:32:23 +0545

    added main providers

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,17 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
+provider "aws" {
+  alias  = "northvrigina"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "ireland"
+  region = "eu-west-1"
+}
+
+provider "aws" {
+  alias  = "london"
+  region = "eu-west-2"
+}
 
 terraform {
   backend "s3" {
@@ -31,4 +42,8 @@ module "cloudfront_distribution" {
   source          = "./modules/cloudfront"
   bucket_1_domain = "${module.website_frontend_bucket_ireland.s3_domain_name}"
   bucket_2_domain = "${module.website_frontend_bucket_london.s3_domain_name}"
+
+  providers = {
+    aws = "aws.northvrigina"
+  }
 }
