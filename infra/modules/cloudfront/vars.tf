edafbec6e7e1dec9c2666b8bfde7bac819ec71c6
karmi214 <karmi214@gmail.com>
2019-09-09 19:15:04 +0545

    acm changed

--- infra/modules/cloudfront/vars.tf
+++ infra/modules/cloudfront/vars.tf
@@ -18,5 +18,5 @@ variable "url_name" {
 }
 
 variable "certificate_arn" {
-  default = "arn:aws:acm:us-east-1:031342435657:certificate/b39dd996-c66b-4ce0-9bc5-f56087dc56df"
+  default = "arn:aws:acm:us-east-1:031342435657:certificate/a7bdc399-76c0-497d-8528-d11bb4963ede"
 }
